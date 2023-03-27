a8d629868889ce4112a77744f2c3a38543071e80
nikef84 <nikzhadec@yandex.ru>
2023-03-27 16:42:43 +0300

    interuption to contol speed

--- tests/servo_test.c
+++ tests/servo_test.c
@@ -1,9 +1,11 @@
 #include "ch.h"
 #include "hal.h"
 #include "common.h"
+#include "terminal_write.h"
 
 
 static PWMDriver *pwm3Driver = &PWMD3;
+bool flag = false;
 
 PWMConfig pwm3conf = {
     .frequency = 500000,
@@ -19,6 +21,13 @@ PWMConfig pwm3conf = {
     .dier       = 0
 };
 
+void palcb_button(void* args){
+    args = args;
+    flag = true;
+}
+
+#define SPEED1          900
+#define SPEED2          950
 
 /*
  * E9 - step
@@ -28,11 +37,30 @@ void servo_test(void)
 {
     chSysInit();
     halInit();
+    debugStreamInit();
+    dbgPrintf("Start\r\n");
+
+    palSetPadMode(GPIOC, GPIOC_BUTTON, PAL_MODE_INPUT_PULLDOWN);
+    // Разрешаем прерывание, указываем callbcak функцию и передаем указатель на аргумент
+    palEnablePadEvent(GPIOC, GPIOC_BUTTON, PAL_EVENT_MODE_RISING_EDGE);
+    palSetPadCallback(GPIOC, GPIOC_BUTTON, palcb_button, NULL);
+
+    uint16_t speed = SPEED1;
     palSetLineMode( PAL_LINE( GPIOB, 4 ),  PAL_MODE_ALTERNATE(2) );
     pwmStart( pwm3Driver, &pwm3conf );
-    pwmEnableChannel( pwm3Driver, 0,500);
+    pwmEnableChannel( pwm3Driver, 0,speed);
+    dbgPrintf("speed = %d\r\n", speed);
 
     while (true){
+        if (flag == true){
+            if (speed == SPEED1) speed = SPEED2;
+            else speed = SPEED1;
+            flag = false;
+            pwmEnableChannel (pwm3Driver, 0,speed);
+            palToggleLine(LINE_LED3);
+            dbgPrintf("speed = %d\r\n", speed);
+
+        }
         chThdSleepMilliseconds(100);
     }
 }
